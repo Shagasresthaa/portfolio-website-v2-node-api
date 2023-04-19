3d2e935a61022fea58f7ce50dc012d76267e2cbf
Shaga Sresthaa <shagasresthaa@gmail.com>
2023-04-19 13:36:12 +0530

    added skeleton base app and added basic login and register endpoints with password hashing and DB connectivity

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import sequelize from '../config/sequelize';
+import { login, createUser } from './controllers/authController';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/api/login', login);
+app.post('/api/register', createUser); 
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+sequelize.sync().then(() => {
+  console.log('Database tables created or updated');
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
